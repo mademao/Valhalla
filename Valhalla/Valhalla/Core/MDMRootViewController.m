34bbf9b5e8dff026fc58533958bfa117838e58d4
Pluto <ismademao@gmail.com>
2018-08-10 21:04:44 +0800

    First Commit

---
+++ Valhalla/Valhalla/Core/MDMRootViewController.m
@@ -0,0 +1,36 @@
+//
+//  MDMRootViewController.m
+//  Valhalla
+//
+//  Created by mademao on 2018/8/10.
+//  Copyright © 2018年 mademao. All rights reserved.
+//
+
+#import "MDMRootViewController.h"
+
+@interface MDMRootViewController ()
+
+@end
+
+@implementation MDMRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
