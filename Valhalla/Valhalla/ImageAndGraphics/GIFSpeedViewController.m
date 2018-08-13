98751614ac39eac1aa632889d4eddb26aa06f4cd
Pluto <ismademao@gmail.com>
2018-08-13 21:09:27 +0800

    Begin change gif speed

--- Valhalla/Valhalla/ImageAndGraphics/GIFSpeedViewController.m
+++ Valhalla/Valhalla/ImageAndGraphics/GIFSpeedViewController.m
@@ -10,6 +10,8 @@
 
 @interface GIFSpeedViewController ()
 
+@property (nonatomic, strong) UIButton *downloadButton;
+
 @end
 
 @implementation GIFSpeedViewController
@@ -18,6 +20,51 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.downloadButton];
+}
+
+- (void)downloadImage {
+    UIActivityIndicatorView *activetyIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activetyIndicatorView.center = self.view.center;
+    activetyIndicatorView.hidesWhenStopped = YES;
+    [self.view addSubview:activetyIndicatorView];
+    [activetyIndicatorView startAnimating];
+    return;
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:@"https://raw.githubusercontent.com/mademao/Resources/master/img/fish.gif"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [session finishTasksAndInvalidate];
+            [activetyIndicatorView stopAnimating];
+            if (data) {
+                UIImage *image = [UIImage imageWithData:data scale:[UIScreen mainScreen].scale];
+                NSLog(@"123");
+            } else {
+                
+            }
+        });
+    }];
+    [dataTask resume];
+}
+
+
+#pragma mark - lazy load
+
+- (UIButton *)downloadButton {
+    if (!_downloadButton) {
+        CGRect bounds = [UIScreen mainScreen].bounds;
+        _downloadButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _downloadButton.frame = CGRectMake(0, 0, bounds.size.width - 100, 50);
+        _downloadButton.center = self.view.center;
+        [_downloadButton setTitle:NSLocalizedString(@"Download GIF", nil) forState:UIControlStateNormal];
+        [_downloadButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        _downloadButton.titleLabel.font = [UIFont systemFontOfSize:25];
+        _downloadButton.layer.borderColor = [UIColor blackColor].CGColor;
+        _downloadButton.layer.borderWidth = 1.0;
+        _downloadButton.layer.cornerRadius = 5.0;
+        [_downloadButton addTarget:self action:@selector(downloadImage) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _downloadButton;
 }
 
 @end
