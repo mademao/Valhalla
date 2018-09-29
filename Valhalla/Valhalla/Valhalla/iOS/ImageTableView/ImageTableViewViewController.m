750249ca834c0ab2b3a441ef60657c3f184e39a7
Pluto <ismademao@gmail.com>
2018-09-29 20:49:05 +0800

    Setup UI

--- Valhalla/Valhalla/Valhalla/iOS/ImageTableView/ImageTableViewViewController.m
+++ Valhalla/Valhalla/Valhalla/iOS/ImageTableView/ImageTableViewViewController.m
@@ -8,7 +8,11 @@
 
 #import "ImageTableViewViewController.h"
 
-@interface ImageTableViewViewController ()
+static const NSInteger kImageViewTag = 11111;
+
+@interface ImageTableViewViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -18,6 +22,47 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.tableView];
+    
+    UIImage *image = [UIImage imageNamed:@"Valhalla"];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, -image.size.height, self.tableView.plt_width, image.size.height)];
+    imageView.tag = kImageViewTag;
+    imageView.image = image;
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    self.tableView.contentInset = UIEdgeInsetsMake(image.size.height, 0, 0, 0);
+    [self.tableView addSubview:imageView];
+}
+
+
+#pragma mark - UITableViewDelegate & UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 30;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[UITableViewCell plt_cellReuseIdentifier] forIndexPath:indexPath];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld--%ld", indexPath.section, indexPath.row];
+    
+    return cell;
+}
+
+
+#pragma mark - lazy load
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:PltScreenBounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:[UITableViewCell plt_cellReuseIdentifier]];
+    }
+    return _tableView;
 }
 
 @end
