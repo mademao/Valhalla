caec73d0b49f0de0c8f678e563627ba4384c65c5
mademao <ismademao@gmail.com>
2019-07-01 19:57:19 +0800

    Commit cat.gif

---
+++ Valhalla/Valhalla/Valhalla/ImageAndGraphics/CompressImage/CompressImageViewController.h
@@ -0,0 +1,17 @@
+//
+//  CompressImageViewController.h
+//  Valhalla
+//
+//  Created by mademao on 2019/7/1.
+//  Copyright © 2019 mademao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CompressImageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Valhalla/Valhalla/Valhalla/ImageAndGraphics/CompressImage/CompressImageViewController.m
@@ -0,0 +1,25 @@
+//
+//  CompressImageViewController.m
+//  Valhalla
+//
+//  Created by mademao on 2019/7/1.
+//  Copyright © 2019 mademao. All rights reserved.
+//
+
+#import "CompressImageViewController.h"
+
+@interface CompressImageViewController ()
+
+@end
+
+@implementation CompressImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+}
+
+@end
