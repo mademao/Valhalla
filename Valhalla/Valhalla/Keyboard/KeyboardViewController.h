2f2ff2437ed19218591362e587ca3ecb7a9368c6
mademao <ismademao@gmail.com>
2021-07-28 12:28:22 +0800

    为Valhalla增加键盘target

---
+++ Valhalla/Valhalla/Keyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  Keyboard
+//
+//  Created by mademao on 2021/7/28.
+//  Copyright © 2021 mademao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
